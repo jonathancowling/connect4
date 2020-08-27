acb37ffbb7594b8b9283a95844c93006ca63700d
Jonathan Cowling <jonathan.cowling@infinityworks.com>
2020-08-27 09:49:57 +0100

    Initial Commit

---
+++ static/game.js
@@ -0,0 +1,5 @@
+// TODO: snippet for creating coin
+// let coin = document.querySelector('#coin-template').content.cloneNode(true)
+// coin.firstElementChild // only usable before in document
+// coin.firstElementChild.style.backgroundColor = 'red'
+// document.querySelector('#main-game').appendChild(coin)
---
+++ static/game.test.js
@@ -0,0 +1,24 @@
+
+test.todo('initCoin creates a red coin')
+
+test.todo('initCoin creates a yellow coin')
+
+test.todo('dropCoin places coin in correct column')
+
+test.todo('dropCoin places coin adjacent')
+
+test.todo('dropCoin places coin on top')
+
+test.todo('dropCoin emits error event if coin placed on full column')
+
+test.todo('dropCoin emits error event if coin placed outside of board')
+
+test.todo("checkWin on an empty board doesn't change state")
+
+test.todo("checkWin on a board with 1 red doesn't change state")
+
+test.todo("checkWin on a board with 4 reds not in a row doesn't change state")
+
+test.todo("checkWin on a board with 4 reds in a row changes state")
+
+test.todo("checkWin on a board with 4 yellows in a row changes state")
